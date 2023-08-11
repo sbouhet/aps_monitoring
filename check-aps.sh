a54d8e8e27cafcb11f341eb6a60161a263fbdd66
sbouhet <sbouhet@gmail.com>
2023-08-11 16:49:35 +0200

    Fix parameter

--- check-aps.sh
+++ check-aps.sh
@@ -2,5 +2,5 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 vars=$SCRIPTPATH"/export-vars.sh"
 source $vars
-cmd="/usr/bin/python "$SCRIPTPATH"/main.py --action check --username "$APSYSTEMS_USER" --pwd "$APSYSTEMS_PWD" --key "$PUSHSAFER_KEY
+cmd="/usr/bin/python "$SCRIPTPATH"/main.py --action check --username "$APSYSTEMS_USER" --pwd "$APSYSTEMS_PWD" --notifier_key "$PUSHSAFER_KEY
 $cmd
